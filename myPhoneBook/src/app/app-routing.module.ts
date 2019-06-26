392cb95711ee451f05d86c7da71b45e32dc250e7
Stephie L'or <stephielor@hotmail.com>
2019-06-27 01:25:26 +0200

    review

--- myPhoneBook/src/app/app-routing.module.ts
+++ myPhoneBook/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ImportantNumbersComponent } from './important-numbers/important-numbers.component';
+import { MyPhoneBookComponent } from './my-phone-book/my-phone-book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+	path:'', component: HomePageComponent
+},
+{
+	path:'important', component: ImportantNumbersComponent
+},
+{
+	path:'phonebook', component: MyPhoneBookComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
